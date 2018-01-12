8b0165abc21002cf81b27dbe67df83ff9a00cd81
liufangshan <360401848@qq.com>
2018-01-12 17:19:28 +0800

    分类列表页..展示及分页

--- application/admin/controller/Category.php
+++ application/admin/controller/Category.php
@@ -1,22 +1,51 @@
 <?php
-namespace app\admin\controller;
-use think\Controller;
 
+namespace app\admin\controller;
+
+use think\Controller;
+use think\Validate;
 class Category extends Controller
 {
+    private $obj;
+    public function _initialize()
+    {
+      $this->obj = model("Category");
+    }
     public function index()
     {
-        return $this->fetch();
+        $parentId=input('get.parent_id',0,'intval');
+        $categorys=$this->obj->getFirstCategory( $parentId);
+        return $this->fetch('',[
+            'categorys'=>$categorys
+            ]);
     }
+
     public function add()
     {
-        return $this->fetch();
+        $category = $this->obj->getNormalFirstCategory();
+        return $this->fetch('',[
+            'category' => $category
+        ]);
     }
+
     public function save()
-    {
-       //print_r($_post)
-       //print_r(input('post.'));
-       print_r(request()->post());
+    { 
+        //print_r($_post)
+        //print_r(input('post.'));
+        //print_r(request()->post());
+                    $data     = input('post.');
+              $data['status'] = 10;
+                    $validate = validate('Category');
+        if (!$validate->scene('add')->check($data)) {
+            $this->error($validate->getError());
+        }
+        //把$data 提交给model层
+        $res = $this->obj->add($data);
+        if($res){
+            $this->success('新增成功');
+        }else{
+            $this->error('新增失败');
+        }
+
     }
 }
-
---
+++ application/common/model/Category.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\common\model;
+
+use think\Model;
+
+class Category extends Model
+{
+    protected $autoWriteTimestamp = true;
+
+    public function add($data)
+    {
+        $data['status'] = 1;
+        // $data['create_time'] = time();
+        return $this->save($data);
+    }
+
+    public function getNormalFirstCategory()
+    {
+        $data = [
+            'status' => 1,
+            'parent_id' => 0,
+        ];
+        $order = [
+            'id' => 'desc',
+        ];
+
+        return $this->where($data)
+        ->order($order)
+        ->select();
+    }
+
+    public function getFirstCategory($parentId = 0)
+    {
+        $data = [
+           'parent_id' =>$parentId,
+           'status' => ['neq', -1],
+       ];
+        $order = [
+           'id' => 'desc',
+       ];
+        $result = $this->where($data)
+       ->order($order)
+      // ->select();
+      //分页
+      ->paginate(1);
+        //echo $this->getLastSql();
+        return $result;
+    }
+}
