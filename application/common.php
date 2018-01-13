73d68226aede8002c6d109d5103b9a46d13a48e9
liufangshan <360401848@qq.com>
2018-01-13 17:49:25 +0800

    百度地图api封装

--- application/common.php
+++ application/common.php
@@ -22,3 +22,28 @@ function status($status)
 return $str;
   
 }
+/**
+ * Undocumented function
+ *
+ * @param [type] $url
+ * @param integer $type
+ * @param array $data
+ * @return void
+ */
+function doCurl($url,$type=0,$data=[]){
+    $ch = curl_init();//初始化
+    //设置选项
+    curl_setopt($ch,CURLOPT_URL,$URL);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);//如果成功只返回结果,不把内容输出来
+    curl_setopt($ch,CURLOPT_HEADER,0);//非0输出header头,0不输出
+    if($type==1){
+      //POST
+      curl_setopt($ch,CURLOPT_POST,1);
+      curl_setopt($ch,CURLOPT_POSTFIELDS,$data);//传值
+    }
+    //执行并获取内容
+    $output=curl_exec($ch);
+    //释放curl句柄
+    return  $output;
+}
+ 
