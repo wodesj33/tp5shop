96226a63c7b1fa5e851e03a5d49227bca93fa585
wodes <wodesj33@163.com>
2018-01-14 23:02:19 +0800

    商户入驻平台模板搭建

---
+++ application/bis/controller/Login.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\bis\controller;
+use think\Controller;
+class Login extends controller
+{
+  public function index()
+    {
+        return $this->fetch();
+        # code...
+    }
+
+}
