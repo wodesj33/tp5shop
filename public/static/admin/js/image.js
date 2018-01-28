5c1caf9a05e000fdc0385d33f7439f9843daf9c2
wodes <wodesj33@163.com>
2018-01-28 21:32:34 +0800

    图片上传  有bug 谷歌不能调用,火狐可调用,但不能显示上传成功..

---
+++ public/static/admin/js/image.js
@@ -0,0 +1,35 @@
+$(function() {
+    $("#file_upload").uploadify({
+        'swf'             : SCOPE.uploadify_swf,
+        'uploader'        : SCOPE.image_upload,
+        'buttonText'       :'图片上传',
+        'fileTypeDesc'     : 'Image files',
+        'fileObjName'     : 'file',
+        'fileTypeExts'    : '*.gif;*.jpg;*.png',
+        'onUploadSuccess' : function(file, data, response) {
+           if(response){
+               var obj = Json.parse(data);
+               $("#upload_org_code_img").attr("src",obj.data);
+               $("#file_upload_image").attr("value",obj.data);
+               $("#upload_org_code_img").show();
+           }
+        }
+    });
+
+    $("#file_upload_other").uploadify({
+        'swf'             : SCOPE.uploadify_swf,
+        'uploader'        : SCOPE.image_upload,
+        'buttonText'       :'图片上传',
+        'fileTypeDesc'     : 'Image files',
+        'fileObjName'     : 'file',
+        'fileTypeExts'    : '*.gif;*.jpg;*.png',
+        'onUploadSuccess' : function(file, data, response) {
+           if(response){
+               var obj = Json.parse(data);
+               $("#upload_org_code_img_other").attr("src",obj.data);
+               $("#file_upload_image_other").attr("value",obj.data);
+               $("#upload_org_code_img_other").show();
+           }
+        }
+    });
+});
